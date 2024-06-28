03806bcff97d9d3fa768377b12785a4fab4717a7
Olli-Pekka <7263059+oplehtinen@users.noreply.github.com>
2024-06-28 16:34:09 +0300

    expose modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 pub mod clplayer;
 pub mod clplayermanager;
-pub(crate) mod cltypes;
-pub(crate) mod player;
+pub mod cltypes;
+pub mod player;
 pub mod playermanager;
-pub(crate) mod types;
-pub(crate) mod util;
+pub mod types;
+pub mod util;
